fe1317215b81a9a01b0b1b2887aab9f17410f7e7
soso4ok <nxdgbccvbnnnn@gmail.com>
2026-07-17 12:31:02 +0200

    Add Google Tag Manager (GTM) script and noscript

--- app/layout.tsx
+++ app/layout.tsx
@@ -116,6 +116,23 @@ export default function RootLayout({
       className={`${inter.variable} ${spaceGrotesk.variable} ${blinker.variable}`}
     >
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MMKM83XF"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MMKM83XF');
+          `}
+        </Script>
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=G-LFYKTHJ825`}
           strategy="afterInteractive"
