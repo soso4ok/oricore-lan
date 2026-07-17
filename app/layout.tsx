ff8325ed497e8e10f22aa6320fb4086ac1674e36
soso4ok <nxdgbccvbnnnn@gmail.com>
2026-07-17 15:31:31 +0200

    Fix Tag Assistant string execution with dangerouslySetInnerHTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -117,8 +117,8 @@ export default function RootLayout({
       className={`${inter.variable} ${spaceGrotesk.variable} ${blinker.variable}`}
     >
       <body>
-        <Script id="consent-default" strategy="beforeInteractive">
-          {`
+        <Script id="consent-default" strategy="beforeInteractive" dangerouslySetInnerHTML={{
+          __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('consent', 'default', {
@@ -127,8 +127,8 @@ export default function RootLayout({
               'ad_personalization': 'denied',
               'analytics_storage': 'denied'
             });
-          `}
-        </Script>
+          `
+        }} />
         
         <noscript>
           <iframe
@@ -139,29 +139,29 @@ export default function RootLayout({
           />
         </noscript>
 
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
+        <Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-MMKM83XF');
-          `}
-        </Script>
+          `
+        }} />
 
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=G-LFYKTHJ825`}
           strategy="afterInteractive"
         />
 
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+        <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-LFYKTHJ825');
-          `}
-        </Script>
+          `
+        }} />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
